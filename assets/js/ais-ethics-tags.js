be1201309f0782567fcf089fc8146cb6e11c7b47
Emerson Rocha Luiz <emerson@alligo.com.br>
2019-04-07 21:01:21 -0300

    html-poc (#7), schema (#8), wikidata (#10): testing wikidata API

--- assets/js/ais-ethics-tags.js
+++ assets/js/ais-ethics-tags.js
@@ -7,3 +7,48 @@
 */
 
 console.log('test from ais-ethics-tags.js');
+
+
+
+
+
+// https://www.wikidata.org/w/api.php?action=wbgetentities&sites=enwiki&titles=Dragon&languages=en|pt|es|eo&props=sitelinks|labels|aliases|descriptions&callback=?&format=jsonfm
+// https://www.wikidata.org/w/api.php?action=wbgetentities&sites=enwiki&ids=Q11660&languages=en|pt|es|eo&props=sitelinks|labels|aliases|descriptions&callback=?&format=jsonfm
+
+
+// https://www.wikidata.org/w/api.php?action=wbgetentities&sites=enwiki&languages=en|pt|es|eo&sitefilter=enwiki|eswiki|ptwiki|eowiki&props=sitelinks|labels|aliases|descriptions&callback=?&format=jsonfm&ids=Q11660
+// https://www.wikidata.org/w/api.php?action=wbgetentities&sites=enwiki&languages=en|pt|es|eo&sitefilter=enwiki|eswiki|ptwiki|eowiki&props=sitelinks|labels|aliases|descriptions&callback=?&format=json&ids=Q11660
+
+
+function wikidata(el, items) {
+  if (!items) {
+    items = 'Q11660|Q8458|Q25378861'
+  }
+  $.getJSON({
+    url: "https://www.wikidata.org/w/api.php?action=wbgetentities&sites=enwiki&languages=en|pt|es|eo&sitefilter=enwiki|eswiki|ptwiki|eowiki&props=sitelinks|labels|aliases|descriptions&callback=?&format=json&ids=" + items,
+    // url: "https://www.wikidata.org/w/api.php?action=wbgetentities&sites=enwiki&languages=en%7Cpt%7Ces%7Ceo&sitefilter=enwiki%7Ceswiki%7Cptwiki%7Ceowiki&props=sitelinks%7Clabels%7Caliases%7Cdescriptions&format=json&ids=" + items,
+    cache: true
+  })
+  .done(function (html) {
+    console.log('wikidata done', el, items, html);
+    $(el).append(html);
+  })
+  .fail(function (html) {
+    console.log('wikidata fail', el, items, html);
+    $(el).append(html);
+  })
+}
+
+// wikidata($('.output-test'), 'Q11660');
+
+
+console.log('aaaaaaa');
+
+const wikidataItemsAll = [
+  'Q8458',  // human rights
+  'Q11660', // artificial intelligence
+  'Q7692538', // Technological unemployment
+  'Q25378861' // Lethal autonomous weapon
+];
+
+wikidata($('.output-test'), wikidataItemsAll.join('|'));
